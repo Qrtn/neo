e938581208f26d1b1a14acf3d0bb128b5327b005
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-04-23 14:09:51 -0500

    Fix timer and add jump instruction

--- spimbot.s
+++ spimbot.s
@@ -35,8 +35,6 @@ RESPAWN_INT_MASK        = 0x2000      ## Respawn
 RESPAWN_ACK             = 0xffff00f0  ## Respawn
 
 # our constants
-END_MOVEMENT		= 12
-RETURN_TO_MOVEMENT	= 0
 
 .data
 ### Movement Pattern
@@ -45,10 +43,11 @@ RETURN_TO_MOVEMENT	= 0
 # 2000		UDP, may be contingent on previous check of arena map
 # 3000 + x	Check arena map, x
 # 3000 + y	Check arena map, y
+# 100000 + cm	Set current_move to cm
 # 100000000 + c	Delay c cycles
 
 movement:
-.word 45, 1510, 110000000
+.word	45, 1510, 101000000, 225, 1510, 101000000, 100000
 
 current_move:
 .word 0
@@ -175,10 +174,6 @@ timer_interrupt:
 	lw	$t0, current_move
 
 execute_until_delay:
-	blt	$t0, END_MOVEMENT, execute_parse
-	li	$t0, RETURN_TO_MOVEMENT
-
-execute_parse:
 	lw	$t1, movement($t0)
 
 	add	$t0, $t0, 4
@@ -186,6 +181,7 @@ execute_parse:
 	blt	$t1, 1000, execute_angle
 	blt	$t1, 2000, execute_velocity
 	blt	$t1, 3000, execute_udp
+	blt	$t1, 100000000, execute_jump
 	j	return_delay
 
 execute_angle:
@@ -203,10 +199,16 @@ execute_udp:
 	sw	$zero, SHOOT_UDP_PACKET
 	j	execute_until_delay
 
+execute_jump:
+	sub	$t0, $t1, 100000
+	j	execute_until_delay
+
 return_delay:
 	sw	$t0, current_move
 
-	sub	$t2, $t1, 100000000
+	sub	$t1, $t1, 100000000
+	lw	$t2, TIMER
+	add	$t2, $t2, $t1
 	sw	$t2, TIMER
 
 	j       interrupt_dispatch     # see if other interrupts are waiting
