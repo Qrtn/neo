1e910697a73c3b646c4259067a2320908930ad82
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-04-25 15:25:31 -0500

    Add example puzzles

---
+++ scripts/puzzle/example_puzzles.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from puzzle_dim import PuzzleDimension, dimensions
+
+RAW_BOARDS = ["""
+ 0   1   1   0   1   0
+ 1   1   0   1   1   1
+ 0   0   1   0   1   1
+ 0   1   1   0   1   1
+ 1   1   1   0   1   0
+"""]
+
+puzzles = []
+
+for board in RAW_BOARDS:
+    rows = board.strip().splitlines()
+    board = [[int(i) for i in row.split()] for row in rows]
+
+    num_rows = len(rows)
+    num_cols = len(rows[0].split())
+    num_colors = max(i for row in board for i in row) + 1
+
+    dim = PuzzleDimension(num_rows, num_cols, num_colors)
+
+    puzzles.append((dim, board))
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    pprint(puzzles)
