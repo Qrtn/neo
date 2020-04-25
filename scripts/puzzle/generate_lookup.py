77d0fb8d44ae6e4481d222b927462c80a7b13f49
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-04-25 16:47:41 -0500

    Finish generate_lookup

--- scripts/puzzle/generate_lookup.py
+++ scripts/puzzle/generate_lookup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
+import sys
+import os
 import itertools
-from puzzle_dim import PuzzleDimension, dimensions, encode_puzzle_dim
+
+from puzzle_dim import PuzzleDimension, dimensions, encode_puzzle_dim, \
+    puzzle_dim_bits_to_dim_id
 import solve
 
+scripts_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(scripts_dir)
+import replace_line
+
+
 ENCODED_ROW_WIDTH = 10
+TOP_ROW_DATATYPE = '.short'
+DIM_ID_DATATYPE = '.byte'
 
 def encode_row(puzzle_dim, row):
     # Returns 12-bit wide int
@@ -16,6 +27,12 @@ def encode_row(puzzle_dim, row):
         bits <<= cell_bit_width
 
     return bits
+
+def encode_puzzle_row(puzzle_dim, bottom_row):
+    dim_id_bits = puzzle_dim.dim_id << ENCODED_ROW_WIDTH
+    bottom_row_bits = encode_row(puzzle_dim, bottom_row)
+
+    return dim_id_bits + bottom_row_bits
 
 def generate_bottom_rows(puzzle_dim):
     return itertools.product(range(puzzle_dim.num_colors),
@@ -38,16 +55,81 @@ def generate_puzzle_lookup(puzzle_dims=dimensions):
     lookup_table = []
 
     for dim in puzzle_dims:
-        dim_id_bits = dim.dim_id << ENCODED_ROW_WIDTH
-
         lookup_by_bottom_row = generate_top_rows(dim)
 
         for bottom_row, top_row in lookup_by_bottom_row:
-            bottom_row_bits = encode_row(dim, bottom_row)
             top_row_bits = encode_row(dim, bottom_row)
-
-            full_puzzle_bits = dim_id_bits + bottom_row_bits
+            full_puzzle_bits = encode_puzzle_row(dim, bottom_row)
 
             lookup_table.append((full_puzzle_bits, top_row_bits))
 
     return lookup_table
+
+def list_tuples_to_array(d):
+    size = max(i[0] for i in d) + 1
+
+    arr = [0] * size
+    for key, val in d:
+        arr[key] = val
+
+    return arr
+
+def generate_puzzle_lookup_array():
+    table = generate_puzzle_lookup()
+    return list_tuples_to_array(table)
+
+def generate_dim_id_lookup_array():
+    return list_tuples_to_array(puzzle_dim_bits_to_dim_id)
+
+def generate_data_segment(datatype, array):
+    return datatype + ' ' + ' '.join(str(i) for i in array)
+
+
+if __name__ == '__main__':
+    USAGE = """\
+Example usages:
+
+generate_lookup.py
+    Generates lookup table and outputs to stdout
+
+generate_lookup.py -r
+    Generates lookup table and replaces respective line in spimbot.s
+
+generate_lookup.py -r <assembly_file>
+    Generates lookup table and replaces respective line in <assembly_file>
+
+generate_lookup.py -h
+    Prints usage
+    """
+
+    replace = False
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-h':
+            print(USAGE)
+            sys.exit(0)
+        elif sys.argv[1] == '-r':
+            replace = True
+
+            try:
+                asm_filename = sys.argv[3]
+            except IndexError:
+                scripts_dir = os.path.dirname(__file__)
+                asm_filename = os.path.normpath(os.path.join(scripts_dir, '..', 'spimbot.s'))
+
+            if not os.path.exists(asm_filename):
+                print('No such file', asm_filename)
+                sys.exit(1)
+
+    puzzle_data = generate_data_segment(TOP_ROW_DATATYPE,
+        generate_puzzle_lookup_array())
+
+    dim_id_data = generate_data_segment(DIM_ID_DATATYPE,
+        generate_dim_id_lookup_array())
+
+    if not replace:
+        print(puzzle_data)
+        print(dim_id_data)
+    else:
+        replace_line.replace_in_file(asm_filename, 'puzzle_table:', puzzle_data + '\n')
+        replace_line.replace_in_file(asm_filename, 'dim_table:', dim_id_data + '\n')
