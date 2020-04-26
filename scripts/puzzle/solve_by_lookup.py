85582a15f6810c08442503fb2170695176ca1ad6
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-04-25 20:05:27 -0500

    Fix lookup solver

--- scripts/puzzle/solve_by_lookup.py
+++ scripts/puzzle/solve_by_lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import copy
+from pprint import pprint
 
 from puzzle_general import toggle_lights, chase_lights, check_solution, \
-    toggle_row_lights
+    toggle_row_lights, combine_boards, get_blank_board
 
 from generate_lookup import generate_puzzle_lookup_array, \
     generate_dim_id_lookup_array, encode_puzzle_row, decode_row, \
@@ -14,34 +15,52 @@ dim_id_table = generate_dim_id_lookup_array()
 def solve_by_lookup(puzzle_dim, board):
     board = copy.deepcopy(board)
 
-    _, bottom_row = chase_lights(puzzle_dim, board)
+    first_presses, bottom_row = chase_lights(puzzle_dim, board)
+
+    board = get_blank_board(puzzle_dim)
+    board[-1] = bottom_row
+
     puzzle_bits = encode_puzzle_row(puzzle_dim, bottom_row)
 
     top_row_bits = puzzle_table[puzzle_bits]
     top_row = decode_row(puzzle_dim, top_row_bits)
 
+    #print(puzzle_dim, 'bottom row: ', bottom_row, 'top row: ', top_row)
+
+    top_presses = get_blank_board(puzzle_dim)
+    top_presses[0] = top_row
+
     toggle_row_lights(puzzle_dim, board, 0, top_row)
 
-    presses, bottom_row = chase_lights(puzzle_dim, board)
-    presses[0] = top_row
-    print(bottom_row)
+    second_presses, bottom_row = chase_lights(puzzle_dim, board)
+    assert all(i == 0 for i in bottom_row)
+
+    board = get_blank_board(puzzle_dim)
+    board[-1] = bottom_row
+
+    presses = combine_boards(puzzle_dim, first_presses, top_presses,
+        second_presses)
 
     return presses
 
 
 if __name__ == '__main__':
-    from pprint import pprint
     from example_puzzles import puzzles
     import solve
+
+    ac = True
 
     for puzzle in puzzles:
         lookup_soln = solve_by_lookup(*puzzle)
         real_soln = solve.solve(*puzzle)
 
-        if lookup_soln != real_soln:
-            pprint(lookup_soln)
-            pprint(real_soln)
+        correct = check_solution(*puzzle, lookup_soln)
 
-        print(check_solution(*puzzle, lookup_soln))
+        if correct:
+            print('Correct')
+        else:
+            print('Incorrect')
+            ac = False
 
-    print('All correct')
+    if ac:
+        print('All correct')
