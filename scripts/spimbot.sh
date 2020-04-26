763c69e10571c9127f9270d6a96abd149e6dc784
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-04-26 17:58:49 -0500

    Make script work from any dir

--- scripts/spimbot.sh
+++ scripts/spimbot.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-QtSpimbot -file ../spimbot.s -file2 ../adversary.s $*
+PROJECT_DIR="$(dirname "$0")/.."
+
+if [ "$#" -eq 0 ]; then
+    QtSpimbot -file $PROJECT_DIR/spimbot.s -file2 $PROJECT_DIR/spimbot_slow_solver.s -randommap -randompuzzle
+else
+    QtSpimbot -file $PROJECT_DIR/spimbot.s -file2 $PROJECT_DIR/spimbot_slow_solver.s $*
+fi
+
