3ecba2b1f0a9bb7188b849ad4e7ea8292c6914ba
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-04-25 23:17:06 -0500

    Add non-working puzzle solver in spimbot_lookup

--- spimbot_lookup.s
+++ spimbot_lookup.s
@@ -85,17 +85,25 @@ main:
 	# Puzzle solving
 	la	$s0, puzzle				# Puzzle address
 
-request_puzzle:
+main_request_puzzle:
 	sw	$s0, REQUEST_PUZZLE			# Request puzzle
 
-check_puzzle_available:
-	lw	$t0, has_puzzle				# Kernel writes to has_puzzle when puzzle is available
-	beq	$t0, $zero, check_puzzle_available	# has_puzzle used to communicate between kernel and user
+main_reset_solution:
+	li	$t0, 0					# i = 0
 
-	la	$a0, puzzle				# Load solver arguments
-	la	$a1, solution
-	li	$a2, 0
-	li	$a3, 0
+main_reset_solution_for:
+	beq	$t0, 256, main_reset_solution_for_done	# TODO: CHANGE TO 65
+
+	sb	$zero, solution($t0)			# solution[i] = 0
+
+	add	$t0, $t0, 1				# ++i
+	j	main_reset_solution_for
+
+main_reset_solution_for_done:
+
+main_check_puzzle_available:
+	lw	$t0, has_puzzle				# Kernel writes to has_puzzle when puzzle is available
+	beq	$t0, $zero, main_check_puzzle_available	# has_puzzle used to communicate between kernel and user
 
 	jal	solve					# Call solver
 
@@ -104,7 +112,7 @@ check_puzzle_available:
 
 	sw	$zero, has_puzzle			# Reset has_puzzle
 
-	j	request_puzzle				# User-space code requests and solves puzzles forever
+	j	main_request_puzzle			# User-space code requests and solves puzzles forever
 							# (Kernel code does the moving)
 	jr	$ra
 
@@ -443,9 +451,107 @@ num_cols: .word 0
 num_colors: .word 0
 puzzle_board: .byte 0:256
 
-solution:   .byte 0:256
+solution: .byte 0:256
+
+ENCODED_ROW_WIDTH = 10
 
 .text
+.globl solve
+solve:
+# Precondition: solution is filled with zeroes
+# Remember to clear board inside the request loop so cycles aren't wasted
+	sw	$ra, 0($sp)
+	sw	$s0, 4($sp) 
+	sw	$s1, 8($sp)
+	sw	$s2, 12($sp)
+	sw	$s3, 16($sp)
+	sub	$sp, $sp, 20
+
+	jal	chase_lights
+
+solve_encode_puzzle_dim:
+	lw	$t0, num_rows
+	lw	$t1, num_cols
+	lw	$t2, num_colors
+
+	sub	$t3, $t0, 5			# row_bits = num_rows - 5
+	sll	$t3, $t3, 3			# row_bits <<= 3
+
+	sub	$t4, $t1, 3			# col_bits = num_cols - 4
+	sll	$t4, $t4, 1			# col_bits <<= 1
+
+	sub	$t5, $t2, 2			# color_bits = num_colors - 2
+
+	add	$t6, $t3, $t4			# puzzle_dim_bits = row_bits + col_bits
+	add	$t6, $t6, $t5			# puzzle_dim_bits += color_bits
+
+	lw	$t8, dim_id_table($t6)		# dim_id = dim_id_table[puzzle_dim_bits]
+
+solve_encode_bottom_row:
+	sub	$s1, $t2, 1			# cell_bit_width = num_colors - 1
+	li	$t4, 0				# bottom_row_bits = 0 
+
+	sub	$t5, $t0, 1			# index (last row's index) = num_rows - 1
+	mul	$t5, $t5, $t1			# index *= num_cols
+
+	add	$t6, $t5, $t1			# end_index
+
+solve_encode_bottom_row_for:
+	beq	$t5, $t6, solve_encode_bottom_row_for_done
+
+	sll	$t4, $t4, $s1			# bottom_row_bits <<= cell_bit_width
+	lbu	$t7, puzzle_board($t5)		# state = puzzle_board[index]
+	add	$t4, $t4, $t7			# bottom_row_bits += state
+
+	add	$t5, $t5, 1			# ++index
+	j	solve_encode_bottom_row_for
+
+solve_encode_bottom_row_for_done:
+
+solve_encode_puzzle:
+	sll	$t8, $t8, ENCODED_ROW_WIDTH	# puzzle_bits = dimension_id << 8
+	add	$t8, $t8, $t4			# puzzle_bits += bottom_row_bits
+	sll	$t8, $t8, 1			# puzzle_bits *= 2 (convert to short index)
+
+solve_lookup_top_row_bits:
+	lhu	$s0, puzzle_table($t8)		# top_row_bits = puzzle_table[puzzle_bits]
+
+	li	$s2, 1				# cell_mask = 1
+	and	$s2, $s2, $s1			# cell_mask &= cell_bit_width
+						# (cell_mask = (cell_bit_width == 1) ? 0b1 : 0b11)
+
+	sub	$s3, $t6, 1			# col = num_cols - 1
+
+solve_apply_top_row_for:
+	blt	$s3, 0, solve_apply_top_row_for_done
+
+	li	$a0, 0				# arg 0: row = 0
+	move	$a1, $s3			# arg 1: col
+	and	$a2, $s0, $s2			# arg 2: action_num = top_row_bits & cell_mask
+
+	sb	$a2, solution($s3)		# solution[col] = action_num
+
+	jal	toggle_light			# toggle_light(last_row, col, action_num)
+
+	srl	$s0, $s0, $s1			# top_row_bits >>= cell_bit_width
+
+	sub	$s3, $s3, 1			# --col
+	j	solve_apply_top_row_for
+
+solve_apply_top_row_for_done:
+	jal	chase_lights
+
+	lw	$ra, 0($sp)
+	lw	$s0, 4($sp) 
+	lw	$s1, 8($sp)
+	lw	$s2, 12($sp)
+	lw	$s3, 16($sp)
+	add	$sp, $sp, 20
+
+	jr	$ra
+
+
+.globl chase_lights
 chase_lights:
 	sw	$ra, 0($sp)
 	sw	$s0, 4($sp) 
@@ -479,6 +585,7 @@ chase_lights_col:
 
 	lbu	$t0, solution($s5)
 	add	$t0, $t0, $a2
+	#rem	$t0, $t0, $s2
 	sb	$t0, solution($s5)
 
 	move	$a0, $s3
@@ -502,9 +609,10 @@ chase_lights_row_done:
 	lw	$s3, 16($sp)
 	lw	$s4, 20($sp)
 	lw	$s5, 24($sp)
-	sub	$sp, $sp, 28
+	add	$sp, $sp, 28
 
 	jr	$ra
+
 
 .globl toggle_light
 toggle_light:
@@ -612,437 +720,6 @@ toggle_light_col_less_if:
 	sb	$t6, 0($t5)
 
 toggle_light_end:
-	jr	$ra
-
-
-# const int MAX_GRIDSIZE = 16;
-# struct LightsOut {
-#     int num_rows;
-#     int num_cols;
-#     int num_color;
-#     unsigned char board[MAX_GRIDSIZE*MAX_GRIDSIZE];
-#     bool clue[MAX_GRIDSIZE*MAX_GRIDSIZE]; //(using bytes in SpimBot)
-#     };
-
-# bool solve(LightsOut* puzzle, unsigned char* solution, int row, int col){
-#     int num_rows = puzzle->num_rows; 
-#     int num_cols = puzzle->num_cols;
-#     int num_colors = puzzle->num_colors;
-#     int next_row = ((col == num_cols-1) ? row + 1 : row);
-#     if (row >= num_rows || col >= num_cols) {
-#	   return board_done(num_rows,num_cols,puzzle->board);
-#     }
-#     if (row != 0) {
-#	  int actions = (num_colors - puzzle->board[(row-1)*num_cols + col]) % num_colors;
-#	  solution[row*num_cols + col] = actions;
-#	  toggle_light(row, col, puzzle, actions);
-#	  if (solve(puzzle,solution, next_row, (col + 1) % num_cols)) {
-#	      return true;
-#	  }
-#	  solution[row*num_cols + col] = 0;
-#	  toggle_light(row, col, puzzle, num_colors - actions);
-#	  return false;
-#     }
-# 
-#     for(char actions = 0; actions < num_colors; actions++) {
-#	  solution[row*num_cols + col] = actions;
-#	  toggle_light(row, col, puzzle, actions);
-#	  if (solve(puzzle,solution, next_row, (col + 1) % num_cols)) {
-#	      return true;
-#	  }
-#	  toggle_light(row, col, puzzle, num_colors - actions); 
-#	  solution[row*num_cols + col] = 0;
-#     }
-#     return false;
-# }
-.globl solve
-solve:
-    ## Stack setup
-    ##
-    ## Index 4	Variable puzzle
-    ## Index 0	Variable ra
-	addi	$sp, $sp, -40
-	sw	$ra, 0($sp)
-	sw	$a0, 4($sp)
-	sw	$s0, 8($sp) 
-	sw	$s1, 12($sp)
-	sw	$s2, 16($sp)
-	sw	$s3, 20($sp)
-	sw	$s4, 24($sp)
-	sw	$s5, 28($sp)
-	sw	$s6, 32($sp)
-	sw	$s7, 36($sp)
-    ##
-    ## End stack setup block
-
-    ## Variables corresponding to registers:
-
-    ##
-    ##	  $t0 = tmp_var
-    ##	  $s4 = actions
-    ##	  $s3 = next_row
-    ##	  $s2 = num_colors
-    ##	  $s1 = num_cols
-    ##	  $s0 = num_rows
-    ##	  $s6 = col
-    ##	  $s5 = row
-    ##	  $a3 = col_in
-    ##	  $a2 = row_in
-    ##	  $s7 = solution
-    ##	  $a1 = solution_in
-    ##	  $a0 = puzzle
-    ##
-    ## End aliases
-
-
-
-    # .stackalloc (4)solution
-	# .stackalloc (4)row (4)col
-
-
-	move	$s7, $a1
-	move	$s5, $a2
-	move	$s6, $a3
-
-	# assign  $s0	= *0($a0)
-	lw	$s0, 0($a0)
-	# assign  $s1	= *4($a0)
-	lw	$s1, 4($a0)
-	# assign  $s2 = *8($a0)
-	lw	$s2, 8($a0)
-
-
-solve_next_row_ternary:
-	# assign  $t0 = $s1 - 1
-	addi	$t0, $s1, -1
-	bne	$s6, $t0, solve_next_row_ternary_else
-
-	# assign  $s3 = $s5 + 1
-	addi	$s3, $s5, 1
-	j	solve_next_row_ternary_end
-solve_next_row_ternary_else:
-	# assign  $s3 = $s5
-	move	$s3, $s5
-solve_next_row_ternary_end:
-
-solve_if_done:
-	bge	$s5, $s0, solve_if_done_cond
-	bge	$s6, $s1, solve_if_done_cond
-	j	solve_if_done_skip
-solve_if_done_cond:
-	# return board_done(num_rows,num_cols,puzzle->board);
-	move	$a0, $s0
-	move	$a1, $s1
-	# assign  $a2 = *12($a0)
-	lw	$a2, 4($sp)
-	add	$a2,$a2,12
-
-	jal	solver_board_done
-
-    ## Stack frame teardown block
-    ##
-	lw	$ra, 0($sp)
-	lw	$s0, 8($sp) 
-	lw	$s1, 12($sp)
-	lw	$s2, 16($sp)
-	lw	$s3, 20($sp)
-	lw	$s4, 24($sp)
-	lw	$s5, 28($sp)
-	lw	$s6, 32($sp)
-	lw	$s7, 36($sp)
-	addi	$sp, $sp, 40
-    ##
-    ## End stack teardown
-
-	jr	$ra
-
-solve_if_done_skip:
-#if (row != 0) {
-#	  int actions = (num_colors - puzzle->board[(row-1)*num_cols + col]) % num_colors;
-#	  solution[row*num_cols + col] = actions;
-#	  toggle_light(row, col, puzzle, actions);
-#	  if (solve(puzzle,solution, next_row, (col + 1) % num_cols)) {
-#	      return true;
-#	  }
-#	  solution[row*num_cols + col] = 0;
-#	  toggle_light(row, col, puzzle, num_colors - actions);
-#	  return false;
-    #	  }
-	beq	$s5, $zero, solve_if_row_not_zero_skip
-	sub	$t0, $s5, 1
-	mul	$t0, $t0, $s1
-	add	$t0, $t0, $s6	 # (row-1)*num_cols + col
-	lw	$a0, 4($sp)
-	add	$a0, $a0, 12
-	add	$t2, $t0, $a0	# t0: offset, a0: puzzle->board
-	lbu	$t1, 0($t2)	# puzzle->board[(row-1)*num_cols + col]
-	sub	$t1, $s2, $t1
-	rem	$s4, $t1, $s2	# s4: actions = (num_colors - puzzle->board[(row-1)*num_cols + col]) % num_colors;
-	add	$t0, $t0, $s1 
-	add	$t0, $t0, $s7
-	sb	$s4, 0($t0)	# solution[row*num_cols + col] = actions
-
-
-	move	$a0, $s5
-	move	$a1, $s6
-	lw	$a2, 4($sp)
-	move	$a3, $s4
-	jal	toggle_light	#toggle_light(row, col, puzzle, actions);
-
-	lw	$a0, 4($sp)
-	move	$a1, $s7
-	move	$a2, $s3
-	add	$a3, $s6, 1
-	rem	$a3, $a3, $s1
-	jal	solve		#solve(puzzle,solution, next_row, (col + 1) % num_cols)
-
-	beq	$v0, 0, solve_if_row_not_zero_solved_skip
-    ## Stack frame teardown block
-    ##
-	lw	$ra, 0($sp)
-	lw	$s0, 8($sp) 
-	lw	$s1, 12($sp)
-	lw	$s2, 16($sp)
-	lw	$s3, 20($sp)
-	lw	$s4, 24($sp)
-	lw	$s5, 28($sp)
-	lw	$s6, 32($sp)
-	lw	$s7, 36($sp)
-	addi	$sp, $sp, 40
-    ##
-    ## End stack teardown
-
-	jr	$ra
-
-solve_if_row_not_zero_solved_skip:
-	mul    $t0, $s5, $s1	   
-	add	$t0, $t0, $s6
-	add	$t0, $t0, $s7
-	sb	$zero, 0($t0)	      #		solution[row*num_cols + col] = 0;
-
-	lw	$a2, 4($sp)
-	move	$a0, $s5
-	move	$a1, $s6
-	sub	$a3, $s2, $s4
-	jal	toggle_light	#toggle_light(row, col, puzzle, num_colors - actions);
-
-	move	$v0, $zero	    # return false
-    ## Stack frame teardown block
-    ##
-	lw	$ra, 0($sp)
-	lw	$s0, 8($sp) 
-	lw	$s1, 12($sp)
-	lw	$s2, 16($sp)
-	lw	$s3, 20($sp)
-	lw	$s4, 24($sp)
-	lw	$s5, 28($sp)
-	lw	$s6, 32($sp)
-	lw	$s7, 36($sp)
-	addi	$sp, $sp, 40
-    ##
-    ## End stack teardown
-
-	jr	$ra
-solve_if_row_not_zero_skip:
-
-	# Saving things to the stack
-	sw	$a0, 4($sp) # sstk    $puzzle, puzzle
-
-	li	$s4, 0
-solve_for_actions:
-	bge	$s4, $s2, solve_for_actions_end
-
-	# assign  $s4 =>:: $s7&[$s5 * $s1 + $s6]
-	mul	$t9, $s5, $s1
-	add	$t9, $t9, $s6
-	add	$t9, $t9, $s7
-	sb	$s4, 0($t9)
-
-	# toggle_light(row, col, puzzle, actions);
-	move	$a0, $s5
-	move	$a1, $s6
-	lw	$a2, 4($sp) # lstk    $a2, puzzle
-	move	$a3, $s4
-	jal	toggle_light
-
-    # if (solve(puzzle,solution, next_row, (col + 1) % num_cols)) {
-solve_recurse_if:
-	lw	$a0, 4($sp) # lstk    $a0, puzzle
-	move	$a1, $s7
-	move	$a2, $s3
-	# assign  $a3 = ($s6 + 1) % $s1
-	addi	$a3, $s6, 1
-	div	$a3, $s1
-	mfhi	$a3
-	jal	solve
-
-	beq	$v0, $0, solve_recurse_if_skip
-
-    ## Stack frame teardown block
-    ##
-	lw	$ra, 0($sp)
-	lw	$s0, 8($sp) 
-	lw	$s1, 12($sp)
-	lw	$s2, 16($sp)
-	lw	$s3, 20($sp)
-	lw	$s4, 24($sp)
-	lw	$s5, 28($sp)
-	lw	$s6, 32($sp)
-	lw	$s7, 36($sp)
-	addi	$sp, $sp, 40
-    ##
-    ## End stack teardown
-
-	jr	$ra
-
-solve_recurse_if_skip:
-	# }
-
-	# toggle_light(row, col, puzzle, num_colors - actions);
-	move	$a0, $s5
-	move	$a1, $s6
-	lw	$a2, 4($sp) # lstk    $a2, puzzle
-	# assign  $a3 = $s2 - $s4
-	sub	$a3, $s2, $s4
-	jal	toggle_light
-
-	# assign  $zero =>:: $s7&[$s5 * $s1 + $s6]
-	mul	$t9, $s5, $s1
-	add	$t9, $t9, $s6
-	add	$t9, $t9, $s7
-	sb	$zero, 0($t9)
-
-solve_for_actions_inc:
-	add	$s4, $s4, 1
-	j	solve_for_actions
-solve_for_actions_end:
-
-    # @RETURN $zero
-	move	$v0, $zero
-
-    ## Stack frame teardown block
-    ##
-	lw	$ra, 0($sp)
-	lw	$s0, 8($sp) 
-	lw	$s1, 12($sp)
-	lw	$s2, 16($sp)
-	lw	$s3, 20($sp)
-	lw	$s4, 24($sp)
-	lw	$s5, 28($sp)
-	lw	$s6, 32($sp)
-	lw	$s7, 36($sp)
-	addi	$sp, $sp, 40
-    ##
-    ## End stack teardown
-
-	jr	$ra
-
-
-# void zero_board(int num_rows, int num_cols, unsigned char* solution){
-#     for (int row = 0; row < num_rows; row++) {
-#	  for (int col = 0; col < num_cols; col++) {
-#	      solution[(row)*num_cols + col] = 0;
-#	  }
-#     }
-# }
-.globl solver_zero_board
-solver_zero_board:
-    ## Variables corresponding to registers:
-
-    ##
-    ##	  $t1 = col
-    ##	  $t0 = row
-    ##	  $a2 = solution
-    ##	  $a1 = num_cols
-    ##	  $a0 = num_rows
-    ##
-	## End aliases
-
-
-	li	$t0, 0
-solver_zero_board_for_row:
-	bge	$t0, $a0, solver_zero_board_for_row_end
-
-	li	$t1, 0
-solver_zero_board_for_col:
-	bge	$t1, $a1, solver_zero_board_for_col_end
-
-	# assign  $zero =>:: $a2&[$t0 * $a1 + $t1]
-	mul	$t9, $t0, $a1
-	add	$t9, $t9, $t1
-	add	$t9, $t9, $a2
-	sb	$zero, 0($t9)
-
-	add	$t1, $t1, 1
-	j	solver_zero_board_for_col
-solver_zero_board_for_col_end:
-
-	add	$t0, $t0, 1
-	j	solver_zero_board_for_row
-solver_zero_board_for_row_end:
-
-	jr	$ra
-
-
-# // it just checks if all lights are off 
-# bool board_done(int num_rows, int num_cols,unsigned char* board){ 
-#     for (int row = 0; row < num_rows; row++) {
-#	  for (int col = 0; col < num_cols; col++) {
-#	      if (board[(row)*num_cols + col] != 0) {
-#		  return false;
-#	      }
-#	  }
-#     }
-#     return true;
-# }
-.globl solver_board_done
-solver_board_done:
-    ## Variables corresponding to registers:
-
-    ##
-    ##	  $t2 = condition_val
-    ##	  $t1 = col
-    ##	  $t0 = row
-    ##	  $a2 = board
-    ##	  $a1 = num_cols
-    ##	  $a0 = num_rows
-    ##
-	## End aliases
-
-
-	li	$t0, 0
-solver_board_done_for_row:
-	bge	$t0, $a0, solver_board_done_for_row_end
-
-	li	$t1, 0
-solver_board_done_for_col:
-	bge	$t1, $a1, solver_board_done_for_col_end
-
-	# assign  $t2 = $a2[$t0 * $a1 + $t1]
-	mul	$t2, $t0, $a1
-	add	$t2, $t2, $t1
-	add	$t2, $t2, $a2
-	lb     $t2, 0($t2)
-solver_board_done_if:
-	beq	$t2, $0, solver_board_done_if_skip
-
-	# @RETURN $zero
-	move	$v0, $zero
-	jr	$ra
-
-solver_board_done_if_skip:
-
-	add	$t1, $t1, 1
-	j solver_board_done_for_col
-solver_board_done_for_col_end:
-
-
-	add	$t0, $t0, 1
-	j solver_board_done_for_row
-solver_board_done_for_row_end:
-
-	# @RETURN 1
-	li	$v0, 1
 	jr	$ra
 
 
