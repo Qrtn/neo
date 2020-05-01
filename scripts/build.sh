1b5327ba8621da0683cea2ad839814e19c893e48
Jeffrey Tang <jeffrey.tang80@gmail.com>
2020-05-01 18:57:27 -0500

    Make build script work with non-existent path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 PROJECT_DIR="$(dirname "$0")/.."
+PATH_SCHEMA="$PROJECT_DIR/scripts/path/$1.py"
 
-$PROJECT_DIR/scripts/path/$1.py > $PROJECT_DIR/build/aml/$2.txt
+if [ -f $PATH_SCHEMA ]; then
+    # If path exists then generate aml from it
+    $PATH_SCHEMA > $PROJECT_DIR/build/aml/$2.txt
+fi
+
 $PROJECT_DIR/scripts/assemble.py $PROJECT_DIR/build/aml/$2.txt $PROJECT_DIR/build/iml/$2.json
 $PROJECT_DIR/scripts/combine.py $PROJECT_DIR/base_spimbot.s $PROJECT_DIR/build/iml/$2.json $PROJECT_DIR/build/puzzle.json $PROJECT_DIR/build/mips/$2.s
